c4faa61e6428371ee091807f3bd00986df478bb5
Richard Meaney <Richard Meaney>
2017-07-05 13:35:02 -0400

    dropping initial files

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB-z3KFtMQmAAcK24Nj9liGpwjofdYsduM",
+    authDomain: "trainscheduler-e57cf.firebaseapp.com",
+    databaseURL: "https://trainscheduler-e57cf.firebaseio.com",
+    projectId: "trainscheduler-e57cf",
+    storageBucket: "trainscheduler-e57cf.appspot.com",
+    messagingSenderId: "1072619709864"
+  };
+  firebase.initializeApp(config);
